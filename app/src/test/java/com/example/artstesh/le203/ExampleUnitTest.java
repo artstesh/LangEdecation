e4a5addf5bd1a824fe8f2d3bcca9ee8857e5abd0
artstesh <artstesh@mail.ru>
2016-07-25 08:51:24 +0300

    Commit2

---
+++ app/src/test/java/com/example/artstesh/le203/ExampleUnitTest.java
@@ -0,0 +1,59 @@
+package com.example.artstesh.le203;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    /*@Test
+    public void addition_isCorrect() throws Exception {
+        String tmp1, tmp2, tmp3, tmp4, tmp5, tmp6;
+        tmp1 = arrayText.get((int) (arrayText.size() * Math.random()));
+        tmp2 = arrayText.get((int) (arrayText.size() * Math.random()));
+        tmp3 = arrayText.get((int) (arrayText.size() * Math.random()));
+        tmp4 = arrayText.get((int) (arrayText.size() * Math.random()));
+        tmp5 = arrayText.get((int) (arrayText.size() * Math.random()));
+        tmp6 = null;
+        if (butcounter < arrayText.size())
+        {
+            tmp6 = arrayText.get(butcounter);
+        }
+        if (butcounter >= arrayText.size())
+        {
+            tmp6 = arrayText.get(arrayText.size() - 1);
+        }
+		*//*if(tmp6 != null && tmp6.equalsIgnoreCase("&***&"))
+		{
+			answer_Button("&***&");
+
+		}*//*
+
+        String[] tmpRand = {tmp1, tmp2, tmp3, tmp4, tmp5, tmp6};
+
+        for (int i = 12; i > 0; i--)
+        {
+            int index = (int) (6 * Math.random());
+            int index2 = (int) (6 * Math.random());
+            String shuffle1 = tmpRand[index];
+            String shuffle2 = tmpRand[index2];
+            tmpRand[index] = shuffle2;
+            tmpRand[index2] = shuffle1;
+        }
+
+        but_ans1.setText(tmpRand[0]);
+        but_ans2.setText(tmpRand[1]);
+        but_ans3.setText(tmpRand[2]);
+        but_ans4.setText(tmpRand[3]);
+        but_ans5.setText(tmpRand[4]);
+        but_ans6.setText(tmpRand[5]);
+
+        assertEquals(4, 2 + 2);
+    }*/
+
+
+
+}
+
